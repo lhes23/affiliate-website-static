0e492d7321ea7898ebcf1140db175b5ca84e619b
Lester <32542078+lhes23@users.noreply.github.com>
2023-03-19 14:45:56 +0800

    lower font size in jumbotron

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -20,13 +20,13 @@ const Jumbotron = () => {
           <div className="flex h-full items-center justify-center">
             <div className="text-white p-4">
               <div>
-                <h1 className="mb-4 text-4xl font-extrabold dark:text-white lg:text-6xl">
+                <h1 className="mb-4 text-3xl font-extrabold dark:text-white lg:text-4xl">
                   <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
                     Best Meats
                   </span>{" "}
                   in San Diego, CA
                 </h1>
-                <p className="text-lg font-normal text-white lg:text-3xl dark:text-gray-400 mx-auto my-16">
+                <p className="text-lg font-normal text-white lg:text-xl dark:text-gray-400 mx-auto my-16">
                   Indulge in the exquisite flavor of Wagyu beef, exclusively at
                   our meat shop.
                 </p>
@@ -35,7 +35,7 @@ const Jumbotron = () => {
               <a
                 href=""
                 // type="button"
-                className="hover:scale-120 rounded border-2 border-neutral-50 px-7 pt-[10px] pb-[8px] text-2xl font-medium uppercase leading-normal text-neutral-50 transition duration-300 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                className="hover:scale-120 rounded border-2 border-neutral-50 px-7 pt-[10px] pb-[8px] text-xl font-medium uppercase leading-normal text-neutral-50 transition duration-300 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
                 // data-te-ripple-init
                 // data-te-ripple-color="light"
               >
